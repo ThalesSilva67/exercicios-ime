13fc71ea59161a79c4dbac8930d933e8db7b6447
Thales Oliveira S. Costa <thalesslva67@gmail.com>
2026-04-11 15:09:11 -0300

    Solução para o exercicio 5 da lista de repetições encaixadas

--- src/main/java/com/exercises/java/repeticoesEncaixadas/Exercicio5.java
+++ src/main/java/com/exercises/java/repeticoesEncaixadas/Exercicio5.java
@@ -2,9 +2,9 @@ package com.exercises.java.repeticoesEncaixadas;
 
 import java.util.Scanner;
 /*
-5. Sabe-se que um número da forma n3 é igual a soma de n ímpares consecutivos.
+5. Sabe-se que um número da forma n³ é igual a soma de n ímpares consecutivos.
 
-Exemplo: 13= 1, 23= 3+5, 33= 7+9+11,  43= 13+15+17+19,...
+Exemplo: 1³= 1, 2³= 3+5, 3³= 7+9+11,  4³= 13+15+17+19,...
 Dado m, determine os ímpares consecutivos cuja soma é igual a n3 para n assumindo valores de 1 a m.
  */
 public class Exercicio5 {
